e936e1a4926330166b54316b3caaa59e7678fa9c
Fernando Rodriguez <frodriguez@Fernandos-MacBook-Pro.local>
2021-10-21 01:33:42 -0500

    fix imput & delete modal

--- frontend/delete.js
+++ frontend/delete.js
@@ -1,7 +1,20 @@
 const deleteBtn = document.getElementById("delete-btn")
+const overlayDelete = document.getElementById("overlay-delete")
+const modalDeleteCancelBtn = document.getElementById("modal-delete-cancel")
+const modalDeleteDeleteBtn = document.getElementById("modal-delete-delete")
 
-deleteBtn.addEventListener("dblclick", function() {
+deleteBtn.addEventListener("click", function(){
+    overlayDelete.classList.remove("hidden")
+})
+
+modalDeleteCancelBtn.addEventListener("click", function(){
+  overlayDelete.classList.add("hidden")
+})
+
+modalDeleteDeleteBtn.addEventListener("click", function() {
   chrome.storage.sync.clear();
   myLeads = []
   render(myLeads)
+  overlayDelete.classList.add("hidden")
+
 })
--- frontend/input.js
+++ frontend/input.js
@@ -1,13 +1,17 @@
-const inputEl = document.getElementById("input-el")
+const urlInput = document.getElementById("input-url")
+const phonesInput = document.getElementById("input-phones")
 const inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function() {
-  newLead.hostName = "Input note: " + inputEl.value
+  newLead.hostName = urlInput.value
+  newLead.hostName = urlInput.value
+  newLead.phones = phonesInput.value.split(",")
   myLeads.unshift(newLead)
   render(myLeads)
   chrome.storage.sync.set({leads:myLeads}, function() {
   //console.log(myLeads)
   })
-  inputEl.value = ""
+  urlInput.value = ""
+  phonesInput.value = ""
   newLead = resetLead()
 });
